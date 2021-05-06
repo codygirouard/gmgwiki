0dafc4b80d6da2954f5d33735adaa6a91aa1e01c
Cody Girouard <codyagirouard@gmail.com>
2021-05-06 05:35:40 -0500

    comments

--- javascript/building.js
+++ javascript/building.js
@@ -1,78 +1,90 @@
 $(document).ready(function(){
+
+  // get value from url ?name=_something_
   const urlParams = new URLSearchParams(window.location.search);
   const buildingName = urlParams.get('name');
 
+  // send get request to server/functions.php
   $.ajax({
-      type: "GET",
-      url: 'php/functions.php',
-      dataType: 'json',
-      data: {function: 'getBuildingInfo', building: buildingName},
+    type: "GET",
+    url: 'php/functions.php',
+    dataType: 'json',
+    // getBuildingInfo(buildingName);
+    data: {function: 'getBuildingInfo', building: buildingName},
 
-      success: function (obj, textstatus) {
-                    $(document).prop('title', obj.name + ' - GMG Wiki');
-                    $("#building-name").html(obj.name);
-                    $("#desc").html(obj.descr);
-                    $("#images-name").html('Images of ' + obj.name);
-                    $("#address").html(obj.addr);
-                    $("#prim-use").html(obj.prim);
-                    $("#academic").html(obj.academic);
+    success: function (obj, textstatus) {
+      // change title of webpage
+      $(document).prop('title', obj.name + ' - GMG Wiki');
 
-                    obj.eagle = obj.eagle.replace(/,/g, ", ");
-                    obj.fs = obj.fs.replace(/,/g, ", ");
-                    obj.fcs = obj.fcs.replace(/,/g, ", ");
-                    obj.rr = obj.rr.replace(/,/g, ", ");
-                    obj.r = obj.r.replace(/,/g, ", ");
-                    obj.ar = obj.ar.replace(/,/g, ", ");
+      // load database information into html
+      $("#building-name").html(obj.name);
+      $("#desc").html(obj.descr);
+      $("#images-name").html('Images of ' + obj.name);
+      $("#address").html(obj.addr);
+      $("#prim-use").html(obj.prim);
+      $("#academic").html(obj.academic);
 
-                    if (obj.eagle == 'None') {
-                      $("#eagle").html('<i class="fa fa-times""></i>');
-                    } else {
-                      $("#eagle").html(obj.eagle);
-                    }
-                    if (obj.fs == 'None') {
-                      $("#fs").html('<i class="fa fa-times""></i>');
-                    } else {
-                      $("#fs").html(obj.fs);
-                    }
-                    if (obj.fcs == 'None') {
-                      $("#fcs").html('<i class="fa fa-times""></i>');
-                    } else {
-                      $("#fcs").html(obj.fcs);
-                    }
-                    if (obj.rr == 'None') {
-                      $("#rr").html('<i class="fa fa-times""></i>');
-                    } else {
-                      $("#rr").html(obj.rr);
-                    }
-                    if (obj.r == 'None') {
-                      $("#r").html('<i class="fa fa-times""></i>');
-                    } else {
-                      $("#r").html(obj.r);
-                    }
-                    if (obj.ar == 'None') {
-                      $("#ar").html('<i class="fa fa-times""></i>');
-                    } else {
-                      $("#ar").html(obj.ar);
-                    }
+      // put spacing in between numbers (1,2,3,4 -> 1, 2, 3, 4)
+      obj.eagle = obj.eagle.replace(/,/g, ", ");
+      obj.fs = obj.fs.replace(/,/g, ", ");
+      obj.fcs = obj.fcs.replace(/,/g, ", ");
+      obj.rr = obj.rr.replace(/,/g, ", ");
+      obj.r = obj.r.replace(/,/g, ", ");
+      obj.ar = obj.ar.replace(/,/g, ", ");
 
-                    if (obj.printer == 1) {
-                      $("#printers").removeClass('fa-times');
-                      $("#printers").addClass('fa-check');
-                    }
-                    if (obj.tutor == 1) {
-                      $("#tutoring").removeClass('fa-times');
-                      $("#tutoring").addClass('fa-check');
-                    }
+      // if no parking spots, html changed to an X
+      if (obj.eagle == 'None') {
+        $("#eagle").html('<i class="fa fa-times""></i>');
+      } else {
+        $("#eagle").html(obj.eagle);
+      }
+      if (obj.fs == 'None') {
+        $("#fs").html('<i class="fa fa-times""></i>');
+      } else {
+        $("#fs").html(obj.fs);
+      }
+      if (obj.fcs == 'None') {
+        $("#fcs").html('<i class="fa fa-times""></i>');
+      } else {
+        $("#fcs").html(obj.fcs);
+      }
+      if (obj.rr == 'None') {
+        $("#rr").html('<i class="fa fa-times""></i>');
+      } else {
+        $("#rr").html(obj.rr);
+      }
+      if (obj.r == 'None') {
+        $("#r").html('<i class="fa fa-times""></i>');
+      } else {
+        $("#r").html(obj.r);
+      }
+      if (obj.ar == 'None') {
+        $("#ar").html('<i class="fa fa-times""></i>');
+      } else {
+        $("#ar").html(obj.ar);
+      }
 
-                    $("#img1").attr("src", "images/" + obj.id + "1.jpg");
-                    $("#img2").attr("src", "images/" + obj.id + "2.jpg");
-                    $("#img3").attr("src", "images/" + obj.id + "3.jpg");
+      // check if building has printers/tutors; true = check, false = X
+      if (obj.printer == 1) {
+        $("#printers").removeClass('fa-times');
+        $("#printers").addClass('fa-check');
+      }
+      if (obj.tutor == 1) {
+        $("#tutoring").removeClass('fa-times');
+        $("#tutoring").addClass('fa-check');
+      }
 
-                    $("#img1").attr("alt", "Picture of " + obj.name);
-                    $("#img2").attr("alt", "Picture of " + obj.name);
-                    $("#img3").attr("alt", "Picture of " + obj.name);
+      // images and alt texts determined by building's id
+      $("#img1").attr("src", "images/" + obj.id + "1.jpg");
+      $("#img2").attr("src", "images/" + obj.id + "2.jpg");
+      $("#img3").attr("src", "images/" + obj.id + "3.jpg");
 
-                    $("#google-map").attr('src', obj.google);
-              }
+      $("#img1").attr("alt", "Picture of " + obj.name);
+      $("#img2").attr("alt", "Picture of " + obj.name);
+      $("#img3").attr("alt", "Picture of " + obj.name);
+
+      // load google maps of building location
+      $("#google-map").attr('src', obj.google);
+    }
   });
 });
--- javascript/main.js
+++ javascript/main.js
@@ -1,36 +1,53 @@
+// wait for page to load
 document.addEventListener('DOMContentLoaded', () => {
   const theme = document.getElementById('theme');
   const themeToggle = document.getElementById('theme-toggle');
+
+  // load saved them if available
   const storedTheme = localStorage.getItem('theme');
   if (storedTheme) {
     theme.href = storedTheme;
   }
 
+  // toggle light mode on button click
   themeToggle.addEventListener('click', () => {
-    // if light mode
     if (theme.href.includes('light')) {
+      // if light mode
       theme.href = 'css/dark.css';
     }
     else {
+      // if dark mode
       theme.href = 'css/light.css';
     }
 
+    // save user preference
     localStorage.setItem('theme', theme.href);
   })
 
+  // filter input box from browse.html
   const filter = document.getElementById('filter');
+  // table of buildings
   const buildings = document.getElementById('buildings');
+  // search input for search.php
   const search = document.getElementById('search-query');
+
+  // list of building names
   var buildingArr = ["AFROTC Building", "Alumni Pavilion", "Apogee Stadium", "Art Building", "Athletic Center Building", "Auditorium-English Building", "Bain Hall", "Bruce Hall", "Business Leadership Building", "Chemistry Building", "Chestnut Hall", "Chilton Hall", "Clark Hall", "Coliseum", "College Inn", "Crumley Hall", "Curry Hall", "Dance and Theater", "Discovery Park Building", "Eagle Landing", "Eagle Student Services Center", "Enviornmental Science Building", "Gateway Center", "General Acadmic Building", "Goolsby Chapel", "Greek Life Center", "Hickory Hall", "Highland Street Parking Garage", "Honors Hall", "Hurley Administration Building", "Joe Greene Hall", "Ken Bahnsen Gym", "Kerr Hall", "Language Building", "Legends Hall", "Library Annex Building", "Life Sciences Complex", "Maple Street Hall", "Marquis Hall", "Matthews Hall", "McConnell Hall", "Mozart Square Hall", "Murchison Performing Arts Center", "Music Building", "Oak Street Hall", "Physical Education Building", "Physics Building", "Pohl Recreation Center", "Radio, TV, Film & Performing Arts", "Rawlins Hall", "Sage Hall", "Santa Fe Squre Hall", "Science Research Building", "Speech & Hearing Clinic", "Sycamore Hall", "Terrill Hall", "Traditions Hall", "Union Circle Parking Garage", "University Union", "UNT Frisco", "Victory Hall", "Welcome Center", "West Hall", "Willis Library", "Wooten Hall"];
+
+  // list of building ids
   var links = ['afrotcbuilding', 'alumnipavilion', 'apogeestadium', 'artbuilding', 'athleticcenterbuilding', 'auditoriumenglishbuilding', 'bainhall', 'brucehall', 'businessleadershipbuilding', 'chemistrybuilding', 'chestnuthall', 'chiltonhall', 'clarkhall', 'coliseum', 'collegeinn', 'crumleyhall', 'curryhall', 'danceandtheater', 'discoveryparkbuilding', 'eaglelanding', 'eaglestudentservicescenter', 'enviornmentalsciencebuilding', 'gatewaycenter', 'generalacadmicbuilding', 'goolsbychapel', 'greeklifecenter', 'hickoryhall', 'highlandstreetparkinggarage', 'honorshall', 'hurleyadministrationbuilding', 'joegreenehall', 'kenbahnsengym', 'kerrhall', 'languagebuilding', 'legendshall', 'libraryannexbuilding', 'lifesciencescomplex', 'maplestreethall', 'marquishall', 'matthewshall', 'mcconnellhall', 'mozartsquarehall', 'murchisonperformingartscenter', 'musicbuilding', 'oakstreethall', 'physicaleducationbuilding', 'physicsbuilding', 'pohlrecreationcenter', 'radiotvfilmperformingarts', 'rawlinshall', 'sagehall', 'santafesqurehall', 'scienceresearchbuilding', 'speechhearingclinic', 'sycamorehall', 'terrillhall', 'traditionshall', 'unioncircleparkinggarage', 'universityunion', 'untfrisco', 'victoryhall', 'welcomecenter', 'westhall', 'willislibrary', 'wootenhall']
+
   if (filter) {
+    // if page is browse.html
     var interval, i, inHTML;
     var oldValue = filter.value;
     var id = '';
 
     inHTML = "<tr><th>Building</th></tr>";
+    // add all buildings into table
     for (i = 0; i < buildingArr.length; i++) {
       if (id === buildingArr[i].charAt(0)) {
+        // first instance of the building's first letter
         inHTML += "<tr><td><a href='building.html?name=" + links[i] + "'>" + buildingArr[i] + "</a></td></tr>";
       }
       else {
@@ -38,38 +55,45 @@ document.addEventListener('DOMContentLoaded', () => {
         inHTML += "<tr id='" + id + "'><td><a href='building.html?name=" + links[i] + "'>" + buildingArr[i] + "</a></td></tr>";
       }
     }
-    document.getElementById("buildings").innerHTML = inHTML;
+    buildings.innerHTML = inHTML;
 
+    // when user has entered the filter input box
     filter.addEventListener('focus', () => {
       interval = setInterval(function(){
-        if (oldValue === filter.value) return;
+        // check user's input every 100 milliseconds
+        if (oldValue === filter.value) return; // user hasn't changed input
         id = '';
         oldValue = filter.value;
         inHTML = "<tr><th>Building</th></tr>";
         for (i = 0; i < buildingArr.length; i++) {
           if (buildingArr[i].toLowerCase().includes(filter.value.toLowerCase())) {
+            // search parameter is found in the building's name
             if (id === buildingArr[i].charAt(0)) {
               inHTML += "<tr><td><a href='building.html?name=" + links[i] + "'>" + buildingArr[i] + "</a></td></tr>";
             }
             else {
+              // first instance of the building's first letter
               id = buildingArr[i].charAt(0);
               inHTML += "<tr id='" + id + "'><td><a href='building.html?name=" + links[i] + "'>" + buildingArr[i] + "</a></td></tr>";
             }
           }
         }
-        document.getElementById("buildings").innerHTML = inHTML;
+        buildings.innerHTML = inHTML;
       }, 100);
     })
 
-    filter.addEventListener('focusout', () => {
+    // stop checking for new input when user left input box
+    filter.addEventListener('focusout', () =>
       clearInterval(interval);
     })
   }
   else if (search) {
+    // if page is search.php
     var i, inHTML = "", x;
     x = 0;
     for (i = 0; i < buildingArr.length; i++) {
       if (buildingArr[i].toLowerCase().includes(search.innerHTML.toLowerCase())) {
+        // search parameter is found with the building's name
         inHTML += "<tr><td><a href='building.html?name=" + links[i] + "'>" + buildingArr[i] + "</a></td></tr>";
         x += 1;
       }
@@ -83,6 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 })
 
+// reset html/css in login modal
 function clearLogin() {
   $("#username").val('');
   $("#pwd").val('');
@@ -93,6 +118,7 @@ function clearLogin() {
   $("#login-button").html("Login");
 }
 
+// check if username is first char = alphabet, rest = alphanumeric
 function checkUser(str) {
   var code, i, len;
 
@@ -112,6 +138,7 @@ function checkUser(str) {
   return true;
 };
 
+// jQuery on page load
 $(document).ready(function(){
   var modal = $("#login");
   var btn = $("#pfp-button");
@@ -119,15 +146,19 @@ $(document).ready(function(){
   var logout = $("#logout");
   var storedUser = localStorage.getItem('user');
 
+  // if user is logged in, color in the profile pic
   if (storedUser && storedUser.length > 2) {
     $("#pfp-img").css("color", "#FBFB3C");
   }
 
+  // pfp button is clicked
   btn.click(function() {
-    storedUser = localStorage.getItem('user');
+    storedUser = localStorage.getItem('user'); // retrieve stored user
     if (!storedUser || storedUser.length < 2) {
+      // if no one is logged in, show login modal
       modal.css("display", "block");
     } else {
+      // someone is logged in, give option to logout
       if ($("#loginDrop").is(":hidden")) {
         $("#loginDrop").show(100);
       } else {
@@ -136,22 +167,29 @@ $(document).ready(function(){
     }
   })
 
+  // user clicks the X in login modal
   exit.click(function() {
+    // modal now hidden
     modal.css("display", "none");
     clearLogin();
   })
 
+  // user clicks logout
   logout.click(function() {
+    // unset user and set pfp image to white
     localStorage.setItem('user', '');
     $("#pfp-img").css("color", "white");
     $("#loginDrop").hide(100);
   })
 
+  // window has been clicked
   $(window).mousedown(function(e) {
     if (e.target.id == 'login') {
+      // user clicked outside of the login modal
       modal.css("display", "none");
       clearLogin();
     } else if (e.target.id != 'pfp-img' && e.target.id != 'logout') {
+      // user clicked outside of the logout dropdown
       $("#loginDrop").hide(100);
     }
   })
@@ -160,36 +198,45 @@ $(document).ready(function(){
   var pwd = $("#pwd");
   var loginbtn = $("#login-button");
 
+  // user tries to login
   loginbtn.click(function() {
     var pass = pwd.val();
     var username = user.val();
 
     if (pass.length >= 6 && username.length >= 2) {
+      // length requirements fulfilled
       $("#pass-error").css("display", "none");
       if (checkUser(username)) {
+        // username is correct format
         $("#user-error").css("display", "none");
         $("#pass-error").css("display", "none");
 
+        // send post request to login
         $.ajax({
           type: "POST",
           url: 'php/functions.php',
           dataType: 'json',
+          // login(username, pass);
           data: {function: 'login', user: username, pwd: pass},
 
           success: function (obj, textstatus) {
             if (obj.error) {
+              // error with login
               if (obj.error.includes('Incorrect Password')) {
+                // incorrect password entered
                 $("#pass-error").css("display", "table-cell");
                 $("#pass-error").html("Wrong Password!");
               }
               else {
+                // unsuccesful attempt at creating user
                 $("#user-error").css("display", "table-cell");
                 $("#user-error").html("Sorry, could not create user!");
               }
             }
             else {
-              localStorage.setItem('user', username);
-              $("#pfp-img").css("color", "#FBFB3C");
+              // login/signup was successful
+              localStorage.setItem('user', username); // set current saved user
+              $("#pfp-img").css("color", "#FBFB3C"); // color in pfp image
               if (obj.new) {
                 // new account created
                 loginbtn.html("Account Created &#10004;");
@@ -199,7 +246,7 @@ $(document).ready(function(){
                 }, 1000);
               }
               else {
-                // login
+                // login successful
                 loginbtn.html("Login Successful &#10004;");
                 setTimeout(function() {
                   modal.css("display", "none");
@@ -209,14 +256,15 @@ $(document).ready(function(){
             }
           }
         });
-
       }
       else {
+        // username not correct format
         $("#user-error").html("Only letters/numbers, first character has to be letter");
         $("#user-error").css("display", "table-cell");
       }
     }
     else {
+      // length requirements failed
       if (pass.length < 6)
       {
         $("#pass-error").css("display", "table-cell");
@@ -230,9 +278,11 @@ $(document).ready(function(){
       }
       else {
         if (checkUser(username)) {
+          // username is correct format
           $("#user-error").css("display", "none");
         }
         else {
+          // username is not correct format
           $("#user-error").html("Only letters/numbers, first character has to be letter");
           $("#user-error").css("display", "table-cell");
         }
